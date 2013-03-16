e8c66653c27e473126306e8e1556151ee0a66f9e
Berat Bilgin <bbilginn@gmail.com>
2013-03-16 08:57:44 +0200

    Açıklamalar eklendi.

--- Nested-Folder-Structure/nfs4v/InitializeNestedViewEngine.vb
+++ Nested-Folder-Structure/nfs4v/InitializeNestedViewEngine.vb
@@ -4,10 +4,6 @@ Imports System.Web
 
 Public NotInheritable Class NestedViewEngine
 
-    Public Sub New()
-        ViewEngines.Engines.Clear()
-    End Sub
-
     Public Shared Function Engine() As RazorViewEngine
 
         Dim vList As New List(Of String)
@@ -17,20 +13,20 @@ Public NotInheritable Class NestedViewEngine
         Dim dir As New DirectoryInfo(HttpContext.Current.Server.MapPath("~/Views/"))
 
         ViewEngines.Engines.Clear()             'Geçmiş siliniyor. -Bu satır olmadan zaten standart yapı dahil olur. Aşağıdaki 3 satıra ihtiyaç olmaz.
-        vList.Add("~/Views/{1}/{0}.vbhtml")     'Standart yapı tanımlanıyor. "~/Views/HomeController/Index.vbhtml"      
-        pList.Add("~/Views/Shared/{0}.vbhtml")  'Standart yapı tanımlanıyor. "~/Views/Shared/_Layout.vbhtml"            
-        mList.Add("~/Views/Shared/{0}.vbhtml")  'Standart yapı tanımlanıyor. "~/Views/Shared/_pLogOnPartial.vbhtml"     
+        vList.Add("~/Views/{1}/{0}.vbhtml")     'Standart yapı ekleniyor. -> "~/Views/Home/Index.vbhtml"      
+        pList.Add("~/Views/Shared/{0}.vbhtml")  'Standart yapı ekleniyor. -> "~/Views/Shared/_Layout.vbhtml"            
+        mList.Add("~/Views/Shared/{0}.vbhtml")  'Standart yapı ekleniyor. -> "~/Views/Shared/_pLogOnPartial.vbhtml"     
 
-        For Each item As DirectoryInfo In dir.GetDirectories
-            vList.Add("~/Views/" + item.Name + "/{1}/{0}.vbhtml")
-            pList.Add("~/Views/" + item.Name + "/_Shared/Partial/{0}.vbhtml")
-            mList.Add("~/Views/" + item.Name + "/_Shared/Layout/{0}.vbhtml")
+        For Each item As DirectoryInfo In dir.GetDirectories ' "~/Views/" klasörü taranıyor.
+            vList.Add("~/Views/" + item.Name + "/{1}/{0}.vbhtml")              'Views altı klasör desenine uygun view yapısı ekleniyor. ->      "~/Views/TESTS/First/Index.vbhtml"
+            pList.Add("~/Views/" + item.Name + "/_Shared/Partial/{0}.vbhtml")  'Views altı klasör desenine uygun partial yapısı ekleniyor. ->   "~/Views/TESTS/_Shared/Partial/_pLogOnPartial.vbhtml"
+            mList.Add("~/Views/" + item.Name + "/_Shared/Layout/{0}.vbhtml")   'Views altı klasör desenine uygun layout yapısı ekleniyor. ->    "~/Views/TESTS/_Shared/Layout/Master1.vbhtml"
         Next
 
         Dim customEngine As New RazorViewEngine
-        customEngine.ViewLocationFormats = vList.ToArray
-        customEngine.PartialViewLocationFormats = pList.ToArray
-        customEngine.MasterLocationFormats = mList.ToArray
+        customEngine.ViewLocationFormats = vList.ToArray          'Location-Format Set ediliyor.
+        customEngine.PartialViewLocationFormats = pList.ToArray   'Location-Format Set ediliyor.
+        customEngine.MasterLocationFormats = mList.ToArray        'Location-Format Set ediliyor.
         Return customEngine
     End Function
 
